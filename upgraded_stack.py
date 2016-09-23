0bfa3916c4927fd1bf401f58035d9d38e6c866f6
roieri100 <roieri100@gmail.com>
2016-09-23 11:32:05 +0300

    Add files via upload

--- upgraded_stack.py
+++ upgraded_stack.py
@@ -8,11 +8,17 @@ class UpgradedStack(Stack):
         self.__max_stack = Stack()
 
     def max_val(self):
-        # return self.__max_stack.peek()
-        pass
+        return self.__max_stack.peek()
 
     def push(self, item):
-        pass
+        super(UpgradedStack, self).push(item)
+        if self.__max_stack.is_empty():
+            self.__max_stack.push(item)
+        else:
+            if item >= self.__max_stack.peek():
+                self.__max_stack.push(item)
 
     def pop(self):
-        pass
+        if super(UpgradedStack, self).peek() == self.__max_stack.peek():
+            self.__max_stack.pop()
+        return self.items.pop()
